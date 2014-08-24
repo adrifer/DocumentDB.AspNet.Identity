b8617893767f1e80b75d3cff1513fef11c406f24
Adrian Fernandez <tracker086@hotmail.com>
2014-08-24 16:11:12 +0200

    Create classes skeleton

---
+++ IdentityUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentDB.AspNet.Identity
+{
+    class IdentityUser : IUser
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets the logins.
+        /// </summary>
+        /// <value>The logins.</value>
+        public virtual List<UserLoginInfo> Logins { get; private set; }
+    }
+}
---
+++ UserStore.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentDB.AspNet.Identity
+{
+    public class UserStore<TUser> : IUserLoginStore<TUser>, IUserClaimStore<TUser>, IUserRoleStore<TUser>,
+       IUserPasswordStore<TUser>, IUserSecurityStampStore<TUser>
+       where TUser : IdentityUser
+    {
+        private bool _disposed;
+
+        public Task AddLoginAsync(TUser user, UserLoginInfo login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TUser> FindAsync(UserLoginInfo login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<UserLoginInfo>> GetLoginsAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveLoginAsync(TUser user, UserLoginInfo login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TUser> FindByIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TUser> FindByNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddClaimAsync(TUser user, System.Security.Claims.Claim claim)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<System.Security.Claims.Claim>> GetClaimsAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveClaimAsync(TUser user, System.Security.Claims.Claim claim)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddToRoleAsync(TUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<string>> GetRolesAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsInRoleAsync(TUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveFromRoleAsync(TUser user, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetPasswordHashAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HasPasswordAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetPasswordHashAsync(TUser user, string passwordHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetSecurityStampAsync(TUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetSecurityStampAsync(TUser user, string stamp)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(GetType().Name);
+            }
+        }
+    }
+}
